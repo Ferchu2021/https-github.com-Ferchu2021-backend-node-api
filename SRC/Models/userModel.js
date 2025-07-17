1fd4591bdfe5c6fd4a17d3a6b6f921bb67729e7f
Fernanda <mariafernandarodriguezuai@gmail.com>
2025-07-16 23:23:47 -0300

    Reemplazar User.js por userModel.js con validaciones mejoradas

---
+++ SRC/Models/userModel.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+  nombre: { /* igual que antes */ },
+  email: { /* igual que antes */ },
+  password: { /* igual que antes */ },
+  fechaCreacion: { /* igual que antes */ },
+  rol: {
+    type: String,
+    enum: ['admin', 'usuario'],
+    default: 'usuario'
+  },
+  estado: {
+    type: Boolean,
+    default: true
+  }
+});
+
+// Hook para hash de contraseña
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
+
